12b56399f649e0b6f6c62eb94e03eb6a1eafc800
Stewart Worrall <stewart.worrall@sydney.edu.au>
2019-05-31 08:26:08 +1000

    Initial commit

--- include/cam_lidar_calibration/main.h
+++ include/cam_lidar_calibration/main.h
@@ -57,60 +57,60 @@ typedef message_filters::Subscriber<sensor_msgs::PointCloud2> pc_sub_type;
 namespace extrinsic_calibration
 {
 
-	class feature_extraction : public nodelet::Nodelet
-	{
-		public:
+class feature_extraction : public nodelet::Nodelet
+{
+public:
 
-  			feature_extraction();
-  			void extractROI(const sensor_msgs::Image::ConstPtr &img, const sensor_msgs::PointCloud2::ConstPtr &pc);
-			void flag_cb(const std_msgs::Int8::ConstPtr& msg);
-			void bounds_callback(cam_lidar_calibration::boundsConfig &config, uint32_t level);
-			double * converto_imgpts(double x, double y, double z);
+  feature_extraction();
+  void extractROI(const sensor_msgs::Image::ConstPtr &img, const sensor_msgs::PointCloud2::ConstPtr &pc);
+  void flag_cb(const std_msgs::Int8::ConstPtr& msg);
+  void bounds_callback(cam_lidar_calibration::boundsConfig &config, uint32_t level);
+  double * converto_imgpts(double x, double y, double z);
 
-		private:
+private:
 
-			struct initial_parameters
-			{
-				std::string camera_topic; 
-				std::string lidar_topic; 
-				bool fisheye_model;
-				int lidar_ring_count;
-				std::pair<int,int> grid_size; 
-				int square_length; // in millimetres
-				std::pair<int,int> board_dimension; // in millimetres
-				std::pair<int,int> cb_translation_error; // in millimetres
-				cv::Mat cameramat;
-				int distcoeff_num;
-				cv::Mat distcoeff;
-			}i_params;
+  struct initial_parameters
+  {
+    std::string camera_topic;
+    std::string lidar_topic;
+    bool fisheye_model;
+    int lidar_ring_count;
+    std::pair<int,int> grid_size;
+    int square_length; // in millimetres
+    std::pair<int,int> board_dimension; // in millimetres
+    std::pair<int,int> cb_translation_error; // in millimetres
+    cv::Mat cameramat;
+    int distcoeff_num;
+    cv::Mat distcoeff;
+  }i_params;
 
-			std::string pkg_loc; 
-			int cb_l, cb_b, l, b, e_l, e_b;	
-			double diagonal;	
+  std::string pkg_loc;
+  int cb_l, cb_b, l, b, e_l, e_b;
+  double diagonal;
 
-			virtual void onInit();
- 
-			ros::Publisher pub;
-			cv::FileStorage fs;
-			int flag = 0; 
-			cam_lidar_calibration::boundsConfig bound;
-			cam_lidar_calibration::calibration_data sample_data;
-			typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::PointCloud2> MySyncPolicy;
-  			message_filters::Synchronizer<MySyncPolicy> *sync;
-			int queue_rate = 5;
-	
-			ros::Publisher roi_publisher;
-			ros::Publisher pub_cloud, expt_region;
-			ros::Publisher vis_pub, visPub;
-  			image_transport::Publisher image_publisher;
-  			ros::Subscriber flag_subscriber;
-			message_filters::Subscriber<sensor_msgs::Image> *image_sub;
-			message_filters::Subscriber<sensor_msgs::PointCloud2> *pcl_sub;
-			visualization_msgs::Marker marker;
-			boost::shared_ptr<image_transport::ImageTransport> it_;
-			boost::shared_ptr<image_transport::ImageTransport> it_p_;
-			boost::shared_ptr<dynamic_reconfigure::Server<cam_lidar_calibration::boundsConfig>> server;
-	};
+  virtual void onInit();
 
-	PLUGINLIB_DECLARE_CLASS(extrinsic_calibration, feature_extraction, extrinsic_calibration::feature_extraction, nodelet::Nodelet);
+  ros::Publisher pub;
+  cv::FileStorage fs;
+  int flag = 0;
+  cam_lidar_calibration::boundsConfig bound;
+  cam_lidar_calibration::calibration_data sample_data;
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::PointCloud2> MySyncPolicy;
+  message_filters::Synchronizer<MySyncPolicy> *sync;
+  int queue_rate = 5;
+
+  ros::Publisher roi_publisher;
+  ros::Publisher pub_cloud, expt_region;
+  ros::Publisher vis_pub, visPub;
+  image_transport::Publisher image_publisher;
+  ros::Subscriber flag_subscriber;
+  message_filters::Subscriber<sensor_msgs::Image> *image_sub;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> *pcl_sub;
+  visualization_msgs::Marker marker;
+  boost::shared_ptr<image_transport::ImageTransport> it_;
+  boost::shared_ptr<image_transport::ImageTransport> it_p_;
+  boost::shared_ptr<dynamic_reconfigure::Server<cam_lidar_calibration::boundsConfig>> server;
+};
+
+PLUGINLIB_DECLARE_CLASS(extrinsic_calibration, feature_extraction, extrinsic_calibration::feature_extraction, nodelet::Nodelet);
 }
--- src/input_sample.cpp
+++ src/input_sample.cpp
@@ -5,54 +5,58 @@
 
 int getch()
 {
-	static struct termios oldt, newt;
-	tcgetattr( STDIN_FILENO, &oldt);           // save old settings
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON);                 // disable buffering 
-	tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
-	int c = getchar();  // read character (non-blocking) 
-	tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
-	return c;
+  static struct termios oldt, newt;
+  tcgetattr( STDIN_FILENO, &oldt);           // save old settings
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON);                 // disable buffering
+  tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
+  int c = getchar();  // read character (non-blocking)
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
+  return c;
 }
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "input_sample");
-	ros::NodeHandle sample("~");
-	ros::Publisher sample_publisher;
-        std_msgs::Int8 flag;  
-	sample_publisher = sample.advertise<std_msgs::Int8>("/flag", 20);
+  ros::init(argc, argv, "input_sample");
+  ros::NodeHandle sample("~");
+  ros::Publisher sample_publisher;
+  std_msgs::Int8 flag;
+  sample_publisher = sample.advertise<std_msgs::Int8>("/flag", 20);
 
-	while(sample_publisher.getNumSubscribers()==0)
-	{
-		ROS_ERROR("Waiting for a subscriber ...");
-		sleep(2);
-	}
-	ROS_INFO("Found a subscriber!"); 
-	std::cout << " Now, press an appropriate key ... " << std::endl; 
-	std::cout << " 'i' to take an input sample" << std::endl; 
-	std::cout << " 'o' to start the optimization process" << std::endl; 
-	std::cout << " 'e' to end the calibration process" << std::endl;
-	ros::spinOnce();
+  while(sample_publisher.getNumSubscribers()==0)
+  {
+    ROS_ERROR("Waiting for a subscriber ...");
+    sleep(2);
+  }
+  ROS_INFO("Found a subscriber!");
+  std::cout << " Now, press an appropriate key ... " << std::endl;
+  std::cout << " 'i' to take an input sample" << std::endl;
+  std::cout << " 'o' to start the optimization process" << std::endl;
+  std::cout << " 'e' to end the calibration process" << std::endl;
+  ros::spinOnce();
 
-	while(ros::ok())
-	{       
-		int c = getch();   
-		if (c == 'i') // flag to take an input sample 
-		{ 
-			flag.data = 1;
-			sample_publisher.publish(flag); 
-			ROS_INFO("sample collected");
-		}    
-		if (c == 'o') // flag to start optimization 
-		{ 
-			flag.data = 2;
-			sample_publisher.publish(flag); 
-			ROS_INFO("starting optimization ...");
-		} 
-		if (c == 'e') // flag to terminate the node; if you do Ctrl+C, press enter after that
-		{
-			ros::shutdown();
-		}
-	}
-	return 0;
+  while(ros::ok())
+  {
+    int c = getch();
+    if (c == 'i') // flag to take an input sample
+    {
+      flag.data = 1;
+      sample_publisher.publish(flag);
+    }
+    if (c == 'o') // flag to start optimization
+    {
+      flag.data = 2;
+      sample_publisher.publish(flag);
+      ROS_INFO("starting optimization ...");
+    }
+    if (c == '\n') // flag to use the input sample
+    {
+      flag.data = 3;
+      sample_publisher.publish(flag);
+    }
+    if (c == 'e') // flag to terminate the node; if you do Ctrl+C, press enter after that
+    {
+      ros::shutdown();
+    }
+  }
+  return 0;
 }
