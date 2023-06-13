158c851b893698ebcf12c93c74c589674415a09d
Julian Gaal <julian.gaal@anavs.de>
2023-06-13 13:37:05 +0200

    fix matplotlib visualization in ROS noetic

--- scripts/visualise_results.py
+++ scripts/visualise_results.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
-import os
-import csv
-import rospkg
-import rospy
-import geometry_msgs.msg
-from tf.transformations import quaternion_from_euler, euler_from_quaternion
-
-from std_msgs.msg import Float64MultiArray
-
-import numpy as np
 import pandas
+import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
-from scipy import stats
+
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+from pathlib import Path
+from multiprocessing import Process
+
 
 # NUM_BINS = 20
 initial_fthresh = 2
@@ -38,11 +36,10 @@ def fit_gauss(df, key, metric, num_bins):
 	return {'key':key, 'metric':metric, 'samples':samples, 'x':x, 'y':y, 'mu':mu, 'sigma':sigma}
 			
 
-def publish_params(gauss):
-
+def publish_calibration(gauss):
 	pub = rospy.Publisher('/extrinsic_calib_param', Float64MultiArray, queue_size=10)
 	rate = rospy.Rate(10)
-	msg = Float64MultiArray()    
+	msg = Float64MultiArray()
 
 	means, stdevs = [], []
 	for param in gauss:
@@ -51,14 +48,14 @@ def publish_params(gauss):
 		means.append(mean)
 		stdevs.append(stdev)
 
-	# Publish data in the order: roll pitch yaw x y z 
-	msg.data = means 
+	# Publish data in the order: roll pitch yaw x y z
+	msg.data = means
 
 	while not rospy.is_shutdown():
 		pub.publish(msg)
 		rate.sleep()
 
-def visualise_results(gauss, nbins_list, degree):
+def visualise_statistics(gauss, nbins_list, degree):
 
 	colors = ["violet","thistle","royalblue","indianred","turquoise","lightslategray"]
 
@@ -69,7 +66,7 @@ def visualise_results(gauss, nbins_list, degree):
 	fig.suptitle('Extrinsic Parameter Results')
 	row = [0,0,0,1,1,1]
 	col = [0,1,2,0,1,2]
-	
+
 	for idx, param in enumerate(gauss):
 		x = param['x']
 		y = param['y']
@@ -80,7 +77,7 @@ def visualise_results(gauss, nbins_list, degree):
 		r = row[idx]
 		c = col[idx]
 		ax[r,c].hist(param['samples'], bins=nbins_list[idx], alpha=0.6, color=colors[idx])
-		ax[r,c].plot(x, y, 'b-', color="black")
+		ax[r,c].plot(x, y, color="black")
 		ax[r,c].set_xlabel(param['key'] + ' (' + param['metric'] + ')')
 		ax[r,c].set_ylabel('Frequency')
 		ax[r,c].set_ylim(0, max(y)+float(max(y))/5)
@@ -100,23 +97,18 @@ def visualise_results(gauss, nbins_list, degree):
 			ax[r,c].annotate(param['key'] + "=% .3f\nstd=% .3f" % (mu, stdev), xy=(mu, max(y)), xytext=(0.94,0.96), **kw)
 		else:
 			ax[r,c].annotate(param['key'] + "=% .5f\nstd=% .5f" % (mu, stdev), xy=(mu, max(y)), xytext=(0.94,0.96), **kw)
-		
-	plt.show(block=False)
+
+	plt.show()
 
 if __name__ == '__main__':
 	rospy.init_node('visualise_results', anonymous=True)
-	rospy.loginfo("Starting visualise_results")
-	path = rospy.get_param("~csv")
+	path = Path(rospy.get_param("~csv"))
 	degree = rospy.get_param("~degree")
 	bin_width_trans = rospy.get_param("~trans_binwidth") # in metres
 	bin_width_rot = rospy.get_param("~rot_binwidth_deg")*np.pi/180 # deg to rads
 
-	if not os.path.exists(path):
+	if not path.exists():
 		raise Exception('GAUSS FITTING - No file found at: {}'.format(path))
-		exit()
-
-	rospy.loginfo("Opening file at: " + path) 
-	rospy.loginfo("Using degrees for rotation")
 
 	# Read data and fit GMM
 	df_orig = pandas.read_csv(path)
@@ -124,6 +116,10 @@ if __name__ == '__main__':
 
 	# Initial filtering of general outliers
 	params = ['roll','pitch','yaw','x','y','z']
+
+	if not all(key in df.columns for key in params):
+		raise Exception("CSV file malformed! Require keys ", params)
+
 	for p in params:
 		df = df[(np.abs(stats.zscore(df[p])) < initial_fthresh)]
 
@@ -136,7 +132,7 @@ if __name__ == '__main__':
 	nbins_list = [num_bins_roll, num_bins_pitch, num_bins_yaw, num_bins_x, num_bins_y, num_bins_z]
 	# print(nbins_list)
 
-	print("\nTotal number of samples: {}".format(len(df)))
+	rospy.loginfo(f"Loaded {len(df)} samples from {path}")
 
 	gauss = []
 	if not degree:
@@ -155,10 +151,13 @@ if __name__ == '__main__':
 	gauss.append(fit_gauss(df, 'y', 'm', num_bins=num_bins_y))
 	gauss.append(fit_gauss(df, 'z', 'm', num_bins=num_bins_z))
 
-	print("\n")     
-	visualise_results(gauss, nbins_list, degree)    
+	print("\n")
 
-	try:
-		publish_params(gauss)
-	except rospy.ROSInterruptException:
-		pass
+	# plt.show(block=False) shows black window, see https://github.com/matplotlib/matplotlib/issues/14873
+	# start plot in a separate python process
+	p = Process(target=visualise_statistics, args=(gauss, nbins_list, degree))
+	p.start()
+
+	publish_calibration(gauss)
+
+	p.join()
